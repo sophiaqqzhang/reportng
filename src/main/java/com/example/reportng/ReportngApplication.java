556ccf1675d7de11087c5c32eaf1ba32beb49af2
Sophia <1035681086@qq.com>
2019-08-14 11:09:54 +0800

    clarify git ignore

---
+++ src/main/java/com/example/reportng/ReportngApplication.java
@@ -0,0 +1,13 @@
+package com.example.reportng;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReportngApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReportngApplication.class, args);
+	}
+
+}
